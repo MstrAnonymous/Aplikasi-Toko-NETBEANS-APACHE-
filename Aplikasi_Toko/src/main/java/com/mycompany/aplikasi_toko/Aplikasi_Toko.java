9aea0d1ecf34941032a892d876b1051386540c9f
MstrAnonymous <127938093+MstrAnonymous@users.noreply.github.com>
2024-08-07 18:28:51 +0700

    Add files via upload

---
+++ Aplikasi_Toko/src/main/java/com/mycompany/aplikasi_toko/Aplikasi_Toko.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplikasi_toko;
+
+
+
+
+/**
+ *
+ * @author Rafiqul
+ */
+
+
+public class Aplikasi_Toko {
+
+    public static void main(String[] args) {
+        new kasir().show();
+    }
+    }
+
